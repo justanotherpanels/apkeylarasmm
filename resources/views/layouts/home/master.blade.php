e5b3c1bdb17567b0cc5050c6c57528600ddea41c
Apkey <apkey@Apkeys-MacBook-Pro.local>
2026-05-28 17:24:50 +0700

    update

--- resources/views/layouts/home/master.blade.php
+++ resources/views/layouts/home/master.blade.php
@@ -16,18 +16,18 @@
 
 
     
-    <title>Productly | Design Agency Landing Page UI</title>
+    <title>Best SMM Panel #1 Cheapest SMM Panel For Reseller</title>
 
 
     <!-- ===============================================-->
     <!--    Favicons-->
     <!-- ===============================================-->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('themes/assets/img/favicons/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('themes/assets/img/favicons/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('themes/assets/img/favicons/favicon-16x16.png') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('themes/assets/img/favicons/favicon.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ $setting && $setting->favicon_path ? $setting->favicon_path : asset('themes/assets/img/favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ $setting && $setting->favicon_path ? $setting->favicon_path : asset('themes/assets/img/favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ $setting && $setting->favicon_path ? $setting->favicon_path : asset('themes/assets/img/favicons/favicon-16x16.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ $setting && $setting->favicon_path ? $setting->favicon_path : asset('themes/assets/img/favicons/favicon.png') }}">
     <link rel="manifest" href="{{ asset('themes/assets/img/favicons/manifest.json') }}">
-    <meta name="msapplication-TileImage" content="{{ asset('themes/assets/img/favicons/mstile-150x150.png') }}">
+    <meta name="msapplication-TileImage" content="{{ $setting && $setting->favicon_path ? $setting->favicon_path : asset('themes/assets/img/favicons/mstile-150x150.png') }}">
     <meta name="theme-color" content="#ffffff">
 
 
@@ -38,6 +38,16 @@
     <link href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
     @stack('styles')
 
+    <!-- PostHog -->
+    <script>
+        !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys getNextSurveyStep onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+        posthog.init('{{ env("POSTHOG_API_KEY", "<ph_project_api_key>") }}', {
+            api_host: '{{ env("POSTHOG_HOST", "https://us.i.posthog.com") }}',
+            person_profiles: 'identified_only'
+        });
+    </script>
+    <!-- End PostHog -->
+    {!! $setting->head_code ?? '' !!}
   </head>
 
 
@@ -69,10 +79,7 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"> </span></button>
           <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto">
-              <li class="nav-item"><a class="nav-link" aria-current="page" href="{{ url('/#feature') }}">Product</a></li>
-              <li class="nav-item"><a class="nav-link" aria-current="page" href="{{ url('/#validation') }}">Customers</a></li>
-              <li class="nav-item"><a class="nav-link" aria-current="page" href="{{ url('/#superhero') }}">Pricing</a></li>
-              <li class="nav-item"><a class="nav-link" aria-current="page" href="{{ url('/#marketing') }}">Resources</a></li>
+              <li class="nav-item"><a class="nav-link" aria-current="page" href="{{ url('/') }}">Home</a></li>
               <li class="nav-item"><a class="nav-link" href="{{ route('services') }}">Services</a></li>
             </ul>
 @if (Route::has('login'))
@@ -99,7 +106,7 @@
 
         <div class="container">
           <div class="row border-top border-top-secondary pt-7">
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-6 mb-lg-0 mb-sm-2 order-1 order-md-1 order-lg-1"><img class="mb-4" src="{{ $setting->logo_path }}" width="184" alt="" /></div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-6 mb-lg-0 mb-sm-2 order-1 order-md-1 order-lg-1"><img class="mb-4" src="{{ $setting && $setting->logo_path ? $setting->logo_path : asset('themes/assets/img/logo.svg') }}" width="184" alt="" /></div>
             <div class="col-lg-3 col-md-6 mb-4 mb-lg-0 order-3 order-md-3 order-lg-2">
               <p class="fs-2 mb-lg-4">Quick Links</p>
               <ul class="list-unstyled mb-0">
@@ -178,6 +185,7 @@
     @stack('scripts')
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&amp;family=Volkhov:wght@700&amp;display=swap" rel="stylesheet">
+    {!! $setting->footer_code ?? '' !!}
   </body>
 
 </html>
